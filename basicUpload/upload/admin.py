ba909048e9f77d013707515a55f6d1ea9b36d16f
Kevin Joshi <kevinjoshi46b@gmail.com>
2022-04-18 12:56:18 +0530

    refactor: fixing directory system for deployement

--- basicUpload/upload/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import modUser
-# Register your models here.
-
-admin.site.register(modUser)
--- basicUpload/upload/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-# Create your models here.
-class modUser(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    typeOfUser = models.BooleanField(null=False, default=False)
