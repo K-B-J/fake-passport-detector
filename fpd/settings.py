d3c842d4527a0d6fee3a8fc664304fab16a02dd5
Kevin Joshi <kevinjoshi46b@gmail.com>
2022-04-20 15:02:10 +0530

    feat: added media directory to the project to store images temporarily

--- fpd/settings.py
+++ fpd/settings.py
@@ -136,6 +136,10 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
